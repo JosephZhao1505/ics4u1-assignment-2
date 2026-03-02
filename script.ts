f2662b7906191ab126724ef471044a3ad66a1038
--replace-all <joseph.zhao150@outlook.com>
2026-03-02 11:40:04 -0500

    Fixed form and began adding trigonometric method for solving

--- script.ts
+++ script.ts
@@ -9,18 +9,25 @@ form?.addEventListener("submit", (event) => {
     const b: number = Number(formData.get("b-value"));
     const c: number = Number(formData.get("c-value"));
     const d: number = Number(formData.get("d-value"));
-    
+
     const p = ((3 * a * c - b * b) / (3 * a * a))
-    const q = ((27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a *a))
+    const q = ((27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a * a))
+    const t = (-b/3*a)
+    console.log(p)
+    console.log(q)
 
     const discriminant = (q / 2) ** 2 + (p / 3) ** 3
+    console.log(discriminant)
 
     if (discriminant < 0) {
-        (document.getElementById("root1") as HTMLInputElement).value = "No Roots";
-    } else if (discriminant > 0) {
-        const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const theta = (1/3)*(Math.acos(q)
+        const rootOne = Math.cbrt((-q / 2) + Math.sqrt(discriminant)) + Math.cbrt((-q / 2) - Math.sqrt(discriminant)) - (b / (3*a));
         const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
-        (document.getElementById("root2") as HTMLInputElement).value = `x1=${rootOne}, x2=${rootTwo}`;
+        const rootThree = (-b - Math.sqrt(discriminant)) / (2 * a);
+        (document.getElementById("root1") as HTMLInputElement).value = `x1=${rootOne}`;
+        (document.getElementById("root2") as HTMLInputElement).value = `x2=${rootTwo}`;
+        (document.getElementById("root3") as HTMLInputElement).value = `x3=${rootThree}`;
+    } else if (discriminant > 0) {
     } else {
         const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
         (document.getElementById("root3") as HTMLInputElement).value = `x=${rootOne}`;
